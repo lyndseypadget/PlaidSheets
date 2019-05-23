f63a6f15ba94909136390136a220a93ccf9110ed
Lyndsey Padget <lpadget@longplaysoftware.com>
2019-05-23 15:11:56 -0500

    Cache access_token and filter transactions by account

--- README.md
+++ README.md
@@ -1,17 +1,37 @@
-# Plaid quickstart
+# PlaidSheets
 
-This repository accompanies Plaid's [**quickstart guide**][quickstart].
+![PlaidSheets](img/plaidsheets.png)
+### A node app using Plaid APIs to get bank transactions into a Google Sheet
 
-Here you'll find full example integration apps using our [**client libraries**][libraries]:
 
-- [Node][node-example]
-- [Ruby][ruby-example]
-- [Python][python-example]
+Based on [Plaid Quickstart](https://plaid.com/docs/quickstart) and adapted for my needs.
 
-![Plaid quickstart app](/assets/quickstart-screenshot.png)
 
-[quickstart]: https://plaid.com/docs/quickstart
-[libraries]: https://plaid.com/docs/libraries
-[node-example]: /node
-[ruby-example]: /ruby
-[python-example]: /python
+Create a .env file with the following contents.  Obtain your own keys [here](https://dashboard.plaid.com/account/keys).
+```
+PLAID_CLIENT_ID="CLIENT_ID"
+PLAID_SECRET="SECRET"
+PLAID_PUBLIC_KEY="PUBLIC_KEY"
+PLAID_ENV="sandbox"
+PLAID_PRODUCTS="transactions"
+```
+
+As per usual, install node modules:
+```
+npm install
+```
+ and run the app with:
+```
+npm install
+```
+or... if you have [nodemon](https://nodemon.io/):
+```
+nodemon ./index.js
+```
+
+Then go to [http://localhost:8000](http://localhost:8000).
+
+### Plaid reference material
+
+#### [Quickstart documentation](https://plaid.com/docs/quickstart)
+#### [Libraries](https://plaid.com/docs/libraries)
---
+++ index.js
@@ -0,0 +1,145 @@
+'use strict';
+
+require('dotenv').config();
+
+var util = require('util');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var moment = require('moment');
+var plaid = require('plaid');
+
+var APP_PORT = process.env.APP_PORT || 8000;
+var PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+var PLAID_SECRET = process.env.PLAID_SECRET;
+var PLAID_PUBLIC_KEY = process.env.PLAID_PUBLIC_KEY;
+var PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
+var NUMBER_OF_DAYS = process.env.NUMBER_OF_DAYS ? parseInt(process.env.NUMBER_OF_DAYS) : 7;
+var DEFAULT_ACCOUNT = process.env.DEFAULT_ACCOUNT;
+var DAYS_TO_CACHE_ACCESS_TOKEN = process.env.DAYS_TO_CACHE_ACCESS_TOKEN ? parseInt(process.env.DAYS_TO_CACHE_ACCESS_TOKEN) : 7;
+
+// PLAID_PRODUCTS is a comma-separated list of products to use when initializing
+// Link. Note that this list must contain 'assets' in order for the app to be
+// able to create and retrieve asset reports.
+var PLAID_PRODUCTS = process.env.PLAID_PRODUCTS || 'transactions';
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+var ACCESS_TOKEN = null;
+var PUBLIC_TOKEN = null;
+var ITEM_ID = null;
+
+// Initialize the Plaid client
+// Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
+var client = new plaid.Client(
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  plaid.environments[PLAID_ENV],
+  {version: '2018-05-22'}
+);
+
+var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response, next) {
+  response.render('index.ejs', {
+    PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
+    PLAID_ENV: PLAID_ENV,
+    PLAID_PRODUCTS: PLAID_PRODUCTS,
+    DAYS_TO_CACHE_ACCESS_TOKEN: DAYS_TO_CACHE_ACCESS_TOKEN
+  });
+});
+
+// Exchange token flow - exchange a Link public_token for
+// an API access_token
+// https://plaid.com/docs/#exchange-token-flow
+app.post('/get_access_token', function(request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error,
+      });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    prettyPrintResponse(tokenResponse);
+    response.json({
+      access_token: ACCESS_TOKEN,
+      item_id: ITEM_ID,
+      error: null,
+    });
+  });
+});
+
+
+// Retrieve Transactions for an Item
+// https://plaid.com/docs/#transactions
+app.get('/transactions', function(request, response, next) {
+  var startDate = moment().subtract(NUMBER_OF_DAYS, 'days').format('YYYY-MM-DD');
+  var endDate = moment().format('YYYY-MM-DD');
+  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    count: 250,
+    offset: 0,
+  }, function(error, transactionsResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error
+      });
+    } else {
+      //Success
+      var account_id;
+      if(DEFAULT_ACCOUNT) {
+        var matchingAcct = transactionsResponse.accounts.find((e) => {
+          return e.mask === DEFAULT_ACCOUNT;
+        });
+
+        if(matchingAcct) {
+          account_id = matchingAcct.account_id;
+        } else {
+          error = 'No account matching the provided mask.  Ensure you have set the last four digits of the desired account correctly with DEFAULT_ACCOUNT="xxxx"';
+          prettyPrintResponse(error);
+          return response.json({
+            error: error
+          });
+        }
+      }
+
+      var filteredTransactions = [];
+      transactionsResponse.transactions.forEach((txn, idx) => {
+        if((account_id && txn.account_id == account_id) || !account_id) {
+          filteredTransactions.push(txn);
+        }
+      });
+      transactionsResponse.transactions = filteredTransactions;
+      prettyPrintResponse(transactionsResponse);
+      response.json({error: null, transactions: transactionsResponse});
+    }
+  });
+});
+
+var server = app.listen(APP_PORT, function() {
+  console.log('PlaidSheets is running on port ' + APP_PORT);
+});
+
+var prettyPrintResponse = response => {
+  // console.log(util.inspect(response, {colors: true, depth: 4}));
+};
+
+app.post('/set_access_token', function(request, response, next) {
+  ACCESS_TOKEN = request.body.access_token;
+  client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
+    response.json({
+      item_id: itemResponse.item.item_id,
+      error: false,
+    });
+  });
+});
